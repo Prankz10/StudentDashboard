82d74d8fab62f8e71b4900fdde758d54afb20846
Prankz10 <catch.pranit1@gmail.com>
2022-12-19 12:38:20 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { StudentService } from './student.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,71 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'StudentDashboard';
+  title = 'studentdashboard';
+
+  studentDetails = null as any;
+  studentToUpdate = {
+    rollNumber:"",
+    name:"",
+    address:"",
+    percentage:""
+  }
+
+  constructor(private service: StudentService) {
+    this.getStudentsDetails();
+  }
+
+  register(registerForm: NgForm) {
+    this.service.registerStudent(registerForm.value).subscribe(
+      (resp) => {
+        console.log(resp);
+        registerForm.reset();
+        this.getStudentsDetails();
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+  getStudentsDetails() {
+    this.service.getStudents().subscribe(
+      (resp) => {
+        console.log(resp);
+        this.studentDetails = resp;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+  deleteStudent(student: any) {
+    this.service.deleteStudent(student.rollNumber).subscribe(
+      (resp) => {
+        console.log(resp);
+        this.getStudentsDetails();
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+  edit(studuent: any){
+    this.studentToUpdate = studuent;
+  }
+
+  updateStudent(){
+    this.service.updateStudents(this.studentToUpdate).subscribe(
+      (resp) => {
+        console.log(resp);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+
 }
